ce48a59ca0c07864ab92cafca3ca302adefad0d9
Colin Hoglund <colinhoglund@gmail.com>
2015-10-10 11:27:47 -0400

    updating install script and readme

--- .install.sh
+++ .install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# Install Homebrew and packages
+# copy git configuration
+if [ ! -f .gitconfig ]; then
+  cp gitconfig .gitconfig
+fi
+
+# install homebrew and packages
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew update
 brew install bash-completion ssh-copy-id wget nmap coreutils vim gnu-sed
+
+# install cluster ssh
 sudo gem install i2cssh
+
+# install slate window manager
 cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 OS X dotfiles
 ========
 
-Clone repo using the following commands:
+Install Xcode command line tools:
 ```
 xcode-select --install
+```
+
+Clone repo using the following commands:
+```
 cd ~/
 git init .
 git remote add origin https://github.com/colinhoglund/dotfiles.git
@@ -14,4 +18,9 @@ git checkout -t origin/master
 Run .install script:
 ```
 ./.install.sh
+```
+
+Enable assitive devices for Slate:
+```
+https://support.apple.com/en-us/HT202866
 ```
