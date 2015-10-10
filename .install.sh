55395cff01ffe5929f3fe1d0ca060936bc5810d9
Colin Hoglund <colinhoglund@gmail.com>
2015-10-10 19:33:50 -0400

    adding more spacing to install script

--- .install.sh
+++ .install.sh
@@ -4,21 +4,21 @@ warn_text="$(tput smul; tput setaf 3)Warning$(tput rmul; tput sgr 0): "
 
 # copy git configuration
 [ -f ~/.gitconfig ]\
- && echo "${warn_text}~/.gitconfig already exists"\
- || cp gitconfig .gitconfig
+  && echo "${warn_text}~/.gitconfig already exists"\
+  || cp gitconfig .gitconfig
 
 # install homebrew and packages
 which brew &> /dev/null\
- && echo "${warn_text}brew already installed"\
- || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  && echo "${warn_text}brew already installed"\
+  || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew update
 brew install bash-completion coreutils git gnu-sed nmap ssh-copy-id tmux vim wget 
 
 # install cluster ssh
 gem list | grep i2cssh &> /dev/null\
- && echo "${warn_text}i2cssh already installed"\
- || sudo gem install i2cssh
+  && echo "${warn_text}i2cssh already installed"\
+  || sudo gem install i2cssh
 
 # install slate window manager
 [ -d /Applications/Slate.app ]\
