f905843acfb14fa077978d69ec58c570e759b1f7
Colin Hoglund <colinhoglund@gmail.com>
2015-10-10 12:08:00 -0400

    adding checks for certain installs

--- .install.sh
+++ .install.sh
@@ -12,7 +12,9 @@ brew update
 brew install bash-completion ssh-copy-id wget nmap coreutils tmux vim gnu-sed
 
 # install cluster ssh
-sudo gem install i2cssh
+gem list | grep i2cssh &> /dev/null || sudo gem install i2cssh
 
 # install slate window manager
-cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
+if [ ! -d /Applications/Slate.app ]; then
+  cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
+fi
