d393934a2e71727dcc4cb40ebbda2fd86a1139b3
Colin Hoglund <colinhoglund@gmail.com>
2015-10-10 12:45:21 -0400

    adding package tests and text formatting

--- .install.sh
+++ .install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+warn_text="$(tput smul; tput setaf 3)Warning$(tput rmul; tput sgr 0): "
+
 # copy git configuration
-if [ ! -f .gitconfig ]; then
-  cp gitconfig .gitconfig
-else
-  echo '~/.gitconfig already exists'
-fi
+[ -f ~/.gitconfig ]\
+ && echo "${warn_text}~/.gitconfig already exists"\
+ || cp gitconfig .gitconfig
 
 # install homebrew and packages
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+which brew &> /dev/null\
+ && echo "${warn_text}brew already installed"\
+ || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew update
 brew install bash-completion ssh-copy-id wget nmap coreutils tmux vim gnu-sed
 
 # install cluster ssh
-gem list | grep i2cssh &> /dev/null && echo 'i2cssh already installed' || sudo gem install i2cssh
+gem list | grep i2cssh &> /dev/null\
+ && echo "${warn_text}i2cssh already installed"\
+ || sudo gem install i2cssh
 
 # install slate window manager
-if [ ! -d /Applications/Slate.app ]; then
-  cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
-else
-  echo 'Slate already installed'
-fi
+[ -d /Applications/Slate.app ]\
+  && echo "${warn_text}Slate already installed"\
+  || $(curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz -C /Applications/)
