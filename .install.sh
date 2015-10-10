9e2784a549ec1779edcfc1255fd72d64a04acd35
Colin Hoglund <colinhoglund@gmail.com>
2015-10-10 12:12:00 -0400

    more checks/output from .install

--- .install.sh
+++ .install.sh
@@ -3,6 +3,8 @@
 # copy git configuration
 if [ ! -f .gitconfig ]; then
   cp gitconfig .gitconfig
+else
+  echo '~/.gitconfig already exists'
 fi
 
 # install homebrew and packages
@@ -12,9 +14,11 @@ brew update
 brew install bash-completion ssh-copy-id wget nmap coreutils tmux vim gnu-sed
 
 # install cluster ssh
-gem list | grep i2cssh &> /dev/null || sudo gem install i2cssh
+gem list | grep i2cssh &> /dev/null && echo 'i2cssh already installed' || sudo gem install i2cssh
 
 # install slate window manager
 if [ ! -d /Applications/Slate.app ]; then
   cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
+else
+  echo 'Slate already installed'
 fi
