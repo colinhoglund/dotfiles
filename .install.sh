7176598ac68d7e4f060d2d50c57e89c4bfea0a62
Colin Hoglund <colinhoglund@gmail.com>
2015-11-08 11:02:36 -0500

    upgrade installed packages when running .install

--- .install.sh
+++ .install.sh
@@ -13,6 +13,7 @@ which brew &> /dev/null\
   || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew update
+brew upgrade --all
 brew install bash-completion coreutils git gnu-sed nmap ssh-copy-id tmux vim wget watch
 
 # install cluster ssh
